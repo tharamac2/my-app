5ca767411e117d1c52a8a2f679e5c142ddc83d02
Tharamac <tharamac2@gmail.com>
2025-11-20 17:27:37 +0530

    updated UI or feature

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,37 +1,20 @@
-import 'package:flutter/material.dart';
-import 'app_routes.dart';
-
-// import your screens
-import 'features/auth/login_page.dart';
-import 'features/auth/signup_page.dart';
+import 'package:go_router/go_router.dart';
+import '../features/auth/login_page.dart';
+import '../features/auth/signup_page.dart';
 
 class AppRouter {
-  static Route<dynamic> generateRoute(RouteSettings settings) {
-    switch (settings.name) {
+  static final GoRouter router = GoRouter(
+    initialLocation: '/login',
 
-      case AppRoutes.splash:
-        return MaterialPageRoute(
-          builder: (_) => const Scaffold(
-            body: Center(child: Text("Splash Loading...")),
-          ),
-        );
-
-      case AppRoutes.login:
-        return MaterialPageRoute(
-          builder: (_) => const LoginPage(),
-        );
-
-      case AppRoutes.signup:
-        return MaterialPageRoute(
-          builder: (_) => const SignupPage(),
-        );
-
-      default:
-        return MaterialPageRoute(
-          builder: (_) => const Scaffold(
-            body: Center(child: Text("Route Not Found")),
-          ),
-        );
-    }
-  }
+    routes: [
+      GoRoute(
+        path: '/login',
+        builder: (context, state) => const LoginPage(),
+      ),
+      GoRoute(
+        path: '/signup',
+        builder: (context, state) => const SignupPage(),
+      ),
+    ],
+  );
 }
